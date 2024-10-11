2bddfe2dd0480ce48864b7754e6834c2b5ef2374
LukasLuut <lukasluut@gmail.com>
2024-10-11 01:13:47 -0300

    adicionado img, sem responsividade

---
+++ inventário/scriptInventario.js
@@ -0,0 +1,21 @@
+function equip(item1,item2){
+item1.src=item2.src
+
+}
+
+
+
+
+
+
+itemPistola={
+    tipo:"Pistola 9mm",
+    capacidadeCarregador:15,
+    municaoAtual:15,
+    reserva:45,
+    src:"./img/escopeta.png"
+}
+
+const equipa=document.getElementById("arma")
+
+equip(equipa,itemPistola)
